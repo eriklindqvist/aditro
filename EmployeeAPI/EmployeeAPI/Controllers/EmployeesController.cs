177ec6fb9e5888f4cbfdd7f0671a09bc9579f4b2
eriklindqvist <erikjo82+github@gmail.com>
2013-11-13 14:49:45 +0100

    Lade till API-controller för employees

---
+++ EmployeeAPI/EmployeeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EmployeeAPI.Models;
+using EmployeeAPI.Database;
+
+namespace EmployeeAPI.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET api/Employees
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET api/Employees/5
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> GetEmployee(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+    }
+}
